4fdb9bbca0533c65ec4a4703b07e8417bec06b02
davidemoro <davide.moro@gmail.com>
2015-06-24 17:57:42 +0200

    removed pdb and old swp

--- kotti_frontend/views.py
+++ kotti_frontend/views.py
@@ -7,5 +7,4 @@ from kotti.resources import Document
 @view_config(route_name='home', renderer='templates/mytemplate.pt')
 def my_view(request):
     one = DBSession.query(Document).first()
-    import pdb; pdb.set_trace()
     return {'one': one, 'project': 'kotti_frontend'}
