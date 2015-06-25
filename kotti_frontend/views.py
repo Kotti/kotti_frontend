1ebbdabe4123500c225d87ad8ce49286ca0f80b7
davidemoro <davide.moro@gmail.com>
2015-06-25 07:44:39 +0200

    update kotti_backend

--- kotti_frontend/views.py
+++ kotti_frontend/views.py
@@ -8,3 +8,8 @@ from kotti.resources import Document
 def my_view(request):
     one = DBSession.query(Document).first()
     return {'one': one, 'project': 'kotti_frontend'}
+
+
+@view_config(context=Document, renderer='templates/mytemplate.pt', permission='pview')
+def document_view(request):
+    return {'one': request.context, 'project': 'kotti_frontend'}
