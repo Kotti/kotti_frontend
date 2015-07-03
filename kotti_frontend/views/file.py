3db644698fc103f6e83438dde2ade455ce2dad29
davidemoro <davide.moro@gmail.com>
2015-07-03 08:15:40 +0200

    refactor file view

--- kotti_frontend/views/file.py
+++ kotti_frontend/views/file.py
@@ -1,13 +1,8 @@
-from pyramid.view import view_config
-
 from kotti.resources import File
 from kotti.views.file import attachment_view
 
 
-@view_config(context=File, permission='pview')
-def file_view(request):
-    return attachment_view(request.context, request)
-
-
 def includeme(config):
-    config.scan(__name__)
+    config.add_view(attachment_view,
+                    context=File,
+                    permission='pview')
