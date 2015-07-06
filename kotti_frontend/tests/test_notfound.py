06dd363a40b50de38e39e3711c134c85a6465217
Davide Moro <davide.moro@gmail,.com>
2015-07-06 11:09:13 +0200

    add test (broken, to be solved asap)

--- kotti_frontend/tests/test_notfound.py
+++ kotti_frontend/tests/test_notfound.py
@@ -3,7 +3,8 @@ class TestNotFound:
     def test_not_found_view_webtest(self, webtest):
         resp = webtest.get('/doesnotexists', status=404)
         assert resp.status_code == 404
-        # TODO: fix me! assert 'kotti_frontend' in resp.body
+        # TODO: fix me!
+        assert 'kotti_frontend' in resp.body
 
     def test_not_found_view(self):
         from kotti_frontend.views.notfound import notfound_view
