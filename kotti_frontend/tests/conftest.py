d333289bf9d5fb713b7f8c51ee41a8d19027474b
Davide Moro <davide.moro@gmail,.com>
2015-07-06 13:32:19 +0200

    fix notfound test broken by previous commit

--- kotti_frontend/tests/conftest.py
+++ kotti_frontend/tests/conftest.py
@@ -8,6 +8,7 @@ from kotti_frontend import frontend_includes
 @fixture(scope='session')
 def custom_settings():
     return {
+        'kotti.site_title': 'kotti_frontend',
         'kotti.use_workflow': 'kotti_frontend:workflows/simple_frontend.zcml',
         'pyramid.includes': ' '.join(frontend_includes),
         }
