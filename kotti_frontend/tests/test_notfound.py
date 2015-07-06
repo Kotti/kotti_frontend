cbdee9bf9321dbf6d6db1c83713965517b690e42
Davide Moro <davide.moro@gmail,.com>
2015-07-06 14:18:06 +0200

    remove TODO comment

--- kotti_frontend/tests/test_notfound.py
+++ kotti_frontend/tests/test_notfound.py
@@ -3,7 +3,6 @@ class TestNotFound:
     def test_not_found_view_webtest(self, webtest):
         resp = webtest.get('/doesnotexists', status=404)
         assert resp.status_code == 404
-        # TODO: fix me!
         assert 'kotti_frontend' in resp.body
 
     def test_not_found_view(self):
