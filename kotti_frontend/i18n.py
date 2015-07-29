b79f6725fcd9e3dfb2c5a68eed4f6e9b3f27c472
Davide Moro <davide.moro@gmail,.com>
2015-07-30 00:51:38 +0200

    add pragma no cover on i18n.py

--- kotti_frontend/i18n.py
+++ kotti_frontend/i18n.py
@@ -1,5 +1,7 @@
-from . import _
+from . import _  # pragma: no cover
 
-# i18n extraction
-_('not_found', default='Not found')
-_('unauthorized', default='Unauthorized')
+
+def i18n():   # pragma: no cover
+    # i18n extraction
+    _('not_found', default='Not found')
+    _('unauthorized', default='Unauthorized')
