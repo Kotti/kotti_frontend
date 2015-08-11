9a8b98c6f09418dceeb88915750990d18b3cb875
Davide Moro <davide.moro@gmail,.com>
2015-08-11 23:40:13 +0200

    avoid kotti_frontend's main and update kotti.base_includes

--- kotti_frontend/tests/conftest.py
+++ kotti_frontend/tests/conftest.py
@@ -2,14 +2,11 @@ from pytest import fixture
 
 pytest_plugins = "kotti"
 
-from kotti_frontend import frontend_includes
-
 
 @fixture(scope='session')
 def custom_settings():
     return {
         'kotti.site_title': 'kotti_frontend',
         'kotti.use_workflow': 'kotti_frontend:workflows/simple_frontend.zcml',
-        'pyramid.includes': ' '.join(frontend_includes),
         'kotti.base_includes': ' '.join(('kotti', 'kotti.views',))
         }
